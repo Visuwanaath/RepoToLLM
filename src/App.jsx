b7f3db9f968a22bc3c66729eb3d84decfa6f6897
Ikko Eltociear Ashimine <eltociear@gmail.com>
2024-08-14 01:05:42 +0900

    chore: update App.jsx
    Github -> GitHub

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <MantineProvider forceColorScheme={'dark'} maw="100%">
       <Analytics/>
-      <Title align="center" mt="xl">{"Github Repo -> LLM"}</Title>
+      <Title align="center" mt="xl">{"GitHub Repo -> LLM"}</Title>
       <Text align="center" mb="xl">Turn your GitHub Repository file structure into JSON to share easily with an LLM</Text>
       <GitHubApiFetcher onFetchComplete={handleFetchComplete} />
       <Container  pos={'fixed'} bottom={0}>
