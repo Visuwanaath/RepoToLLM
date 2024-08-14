d6ca368e8e55ca0770794109e46157b9af2e6ec2
Visuwanaath Selvam <Visuwanaathsh63@gmail.com>
2024-08-13 20:31:23 -0400

    added donation link

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -1,10 +1,11 @@
 import {  Text, Group, ActionIcon } from '@mantine/core';
-import { IconBrandGithub } from '@tabler/icons-react';
+import { IconBrandGithub, IconCoffee } from '@tabler/icons-react';
 
 function AppFooter() {
   return (
       <Group position="center">
         <Text mr={0} pr={0}>Made by Visuwanaath Selvam in 2024</Text>
+        <Text> Source Code: </Text>
           <ActionIcon ml={0} pl={0}
             component="a"
             href="https://github.com/Visuwanaath"
@@ -13,6 +14,16 @@ function AppFooter() {
             variant="transparent"
           >
             <IconBrandGithub size={20} />
+          </ActionIcon>
+          <Text> Buy me a coffee: </Text>
+          <ActionIcon ml={0} pl={0}
+            component="a"
+            href="https://ko-fi.com/visuwa"
+            target="_blank"
+            size="lg"
+            variant="transparent"
+          >
+            <IconCoffee size={20}/>
           </ActionIcon>
       </Group>
   );
